f016d59c7c1c4d4695b855ae93aebfdeab98674e
Cosmina Mihoreanu <cosmina.mihoreanu@gmail.com>
2024-05-16 11:33:38 +0300

    added some dependencies and wiring diagrams

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,14 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("fejfakjdvv");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("qqqqqqqqqq");
+  delay(500);
 }
